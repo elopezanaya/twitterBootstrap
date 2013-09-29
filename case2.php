72043a6eaedbd3b3d9323f8c3ed1d59ef076e269
@iamcoder <eilopeza@hotmail.com>
2013-09-28 20:41:27 -0400

    case2

--- case2.php
+++ case2.php
@@ -1,31 +1,15 @@
-<?php
-    header('Content-type: text/html');
-    header('Access-Control-Allow-Origin:http://api.yoursite.com');
-    ?>
+<?php ?>
 <html>
 <head>
-    
-    <!-- Bootstrap -->
     <link href="css/case2.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen and (min-width: 40.5em)">
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 <script type="text/javascript" src="js/jquery-1.10.2.min.map"></script>
  
     <script src="js/bootstrap.min.js"></script>
-    
 
-    <script>
-   
-    $( document ).ready(function() {
-        console.log( "document loaded" );
-    });
- 
-    $( window ).load(function() {
-        console.log( "window loaded" );
-    });
-    </script></head>
+</head>
 <body>
 <div id="component">
     <script type="text/javascript">
@@ -38,36 +22,14 @@
         console.log($(element).closest('.table').find('.input').val());
         
     }
+    
         function login(element){
             
-            var tmp = $(element).closest("input[type=text]").attr('value');
-  console.log('tmp : ' +tmp);
-            console.log('login');
-            validateFields(element)
-            var data;
-          
-          
             var url= "http://api.yoursite.com/login?username="+$('#username').val()+'&password='+$('#password').val();
         
-  //  httpRequest.onreadystatechange = alertContents;
-    httpRequest.open("GET", url, true);
-    httpRequest.withCredentials = true;
-    httpRequest.send();
-            
-            }
-            
-            function alertContents(){
-               
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        alert(httpRequest.responseText);
-      } else {
-        alert('There was a problem with the request.');
-      }
-    }
-            }
-            
-     
+        
+        
+        
      
             
      requestConfig = {
@@ -90,6 +52,27 @@ contentType: "application/json",
  dataType: 'jsonp'
  });
 })(jQuery);
+        
+        
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open("GET", url, true);
+    httpRequest.withCredentials = true;
+    httpRequest.send();
+            
+            }
+            
+            function alertContents(){
+               
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+            }
+            
+     
     //]]>
     
     
